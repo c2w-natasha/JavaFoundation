a6fb3b7c6eb9eda100a77f0afb46a7df5f5400cd
Natasha Kacharia <natashakacharia@hotmail.com>
2015-07-26 12:31:08 -0700

    EVEN AND ODDS

--- Session8HW/src/com/clubs2win/session8/Main.java
+++ Session8HW/src/com/clubs2win/session8/Main.java
@@ -1,9 +1,23 @@
+
 package com.clubs2win.session8;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //HW for session 8 
+        int n;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        // write your code here
+        //HW for session 8
+        if (n % 2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
     }
 }
+// EVEN OR ODD 
