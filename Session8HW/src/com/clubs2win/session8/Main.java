e56283bc9da5669863e2f296ca7e62f0ff9f39f2
Natasha Kacharia <natashakacharia@hotmail.com>
2015-07-26 12:52:48 -0700

    PALINDROME OR NOT/EVEN OR ODD/PRIME OR NOT

--- Session8HW/src/com/clubs2win/session8/Main.java
+++ Session8HW/src/com/clubs2win/session8/Main.java
@@ -37,5 +37,59 @@ public class Main {
 
 
 
+// EVEN OR ODD
+package com.clubs2win.session8;
+
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        // write your code here
+        //HW for session 8
+        if (n % 2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
+}
+// EVEN OR ODD
+
+
+
+
+package com.clubs2win;
+
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //  boolean isPrime(int x){
+        int x;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        for (int n = 2; n < x; n++) {
+            if (x % n!=0)
+                System.out.println("true");
+            else {
+                System.out.println("false");
+                break;
+            }
+        }
+    }
+}
+// PRIME NUMBER OR NOT.
+
+
+
+
 
 
