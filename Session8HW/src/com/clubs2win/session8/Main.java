bf7d7245e95733c3eb6643008d15d4cb1209c567
Natasha Kacharia <natashakacharia@hotmail.com>
2015-07-26 12:45:45 -0700

    PALINDROME OR NOT

--- Session8HW/src/com/clubs2win/session8/Main.java
+++ Session8HW/src/com/clubs2win/session8/Main.java
@@ -1,34 +1,38 @@
-
-
-
-
-
-// PRIME NUMBER OR NOT.
 package com.clubs2win.session8;
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //  boolean isPrime(int x){
-        int x;
+        int x = 0;
         System.out.println("Enter a number");
         Scanner in = new Scanner(System.in);
         x = in.nextInt();
-        for (int n = 2; n < x; n++) {
-            if (x % n!=0)
-                System.out.println("true");
-            else {
-                System.out.println("false");
-                break;
-            }
+        String str = "" + x;
+        String reverse = "";
+
+        int length = str.length();
+        System.out.println(str);
+        System.out.println(length);
+        //str=Integer.toString(in;
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        System.out.println(reverse);
+        int revlen = reverse.length();
+        System.out.println(revlen);
+        if (str.equals(reverse)) {
+            System.out.println("True");
+
+        }
+        else {
+            System.out.println("False");
+
         }
     }
 }
-
-
-
+//PALINDROME OR NOT
 
 
 
